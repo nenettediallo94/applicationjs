21ea6ee7730e07e6535a3077242fa7b4b8da694b
nenette <nenettediallo94@gmail.com>
2025-07-01 19:01:02 +0200

    supp

--- maino.js
+++ maino.js
@@ -1,14 +1,22 @@
+// main.js
 
 import {
   ajouterUtilisateur,
   listerUtilisateurs,
-
+  supprimerUtilisateur
 } from './users.js';
+
 
 ajouterUtilisateur("Ali");
 ajouterUtilisateur("Seydou");
 ajouterUtilisateur("Fatou");
 
+
 listerUtilisateurs();
 
+
+supprimerUtilisateur("Seydou");
+
+
+listerUtilisateurs();
 
