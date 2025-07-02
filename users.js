12ee4370e0b3d8e12ac52f27d3eba8c0c1212199
nenette <nenettediallo94@gmail.com>
2025-07-02 12:01:50 +0200

    modification

--- users.js
+++ users.js
@@ -15,7 +15,7 @@ export function listerUtilisateurs() {
 export function supprimerUtilisateur(nom) {
   const index = utilisateurs.indexOf(nom);
   if (index !== -1) {
-    utilisateurs.splice(index, 1);
+    utilisateurs.splice(index, 1); 
     console.log(`${nom} a été supprimé.`);
   } else {
     console.log(`${nom} n'existe pas dans la liste.`);
